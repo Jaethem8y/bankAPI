2917b28f972dc0d2654fb0c9b65796c6ede026aa
jaethem8 <jaehyeok.choi@drake.edu>
2022-02-08 21:03:04 -0600

    add front page

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
 from fastapi import FastAPI, Request
 from service.tableNameService import tableNameService
 from postModels.Limit import Limit
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory="templates")
 
-@app.get(path="/")
-async def getIndex():
-  return "Hello"
+
+
+@app.get(path="/",response_class=HTMLResponse)
+async def getIndex(request:Request):
+  return templates.TemplateResponse("index.html",{"request":request})
 
 @app.get(path="/{table_name}")
 async def getByTableName(table_name:str):
