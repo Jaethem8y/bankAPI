f9799c44e8af33b22ef2a0dc82d81a7f1ad0221a
Jacob Danner <jacob.danner@drake.edu>
2022-02-08 18:44:13 -0600

    initial commit

--- repository/singleRepo.py
+++ repository/singleRepo.py
@@ -3,14 +3,14 @@ from sqlalchemy import MetaData, create_engine,Table
 from sqlalchemy.orm import sessionmaker
 from key import key
 
-
+# start and end
 def singleRepo(tableName:str)->object:
   engine = create_engine(key)
   metadata = MetaData(engine)
   Session = sessionmaker(bind=engine)
   session = Session()
   table = Table(tableName,metadata,autoload=True,autoload_with=engine)
-  return session.query(table).all()
+  return session.query(table).all()# [start:end], error handle, internal server error. try except 
 
 
 
--- service/tableNameService.py
+++ service/tableNameService.py
@@ -1,7 +1,9 @@
 from repository.singleRepo import singleRepo
-
+# optional start and end ints
+# default 0 , 1000
+# 
 def tableNameService(tableName:str)->object:
   if tableName == "data_dict" or tableName == "fdic_fail":
-    return singleRepo(tableName)
+    return singleRepo(tableName) # pass start , int, required
   tableNameComplete = "table_"+tableName
   return singleRepo(tableNameComplete)
