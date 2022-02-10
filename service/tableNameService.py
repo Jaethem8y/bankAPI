7a88ce4668c3044f06be88920e6931c1c17cf000
jaethem8 <jaehyeok.choi@drake.edu>
2022-02-10 08:30:04 -0600

    add docker

--- service/tableNameService.py
+++ service/tableNameService.py
@@ -2,7 +2,7 @@ from repository.singleRepo import singleRepo
 # optional start and end ints
 # default 0 , 1000
 # 
-def tableNameService(tableName:str, start:int = 0, end:int = 1000)->object:
+def tableNameService(tableName:str, start:int = 0, end:int = 100000)->object:
   if tableName == "data_dict" or tableName == "fdic_fail":
     return singleRepo(tableName, start, end) # pass start , int, required
   tableNameComplete = "table_"+tableName
