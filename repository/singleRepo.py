cef95aec4a93538cb1ddad1d998451908bff5ef0
Jacob Danner <jacob.danner@drake.edu>
2022-02-09 20:34:36 -0600

    changed parameters for tableNameService

--- repository/singleRepo.py
+++ repository/singleRepo.py
@@ -4,13 +4,13 @@ from sqlalchemy.orm import sessionmaker
 from key import key
 
 # start and end
-def singleRepo(tableName:str)->object:
+def singleRepo(tableName:str, start:int, end:int)->object:
   engine = create_engine(key)
   metadata = MetaData(engine)
   Session = sessionmaker(bind=engine)
   session = Session()
   table = Table(tableName,metadata,autoload=True,autoload_with=engine)
-  return session.query(table).all()# [start:end], error handle, internal server error. try except 
+  return session.query(table)[start:end] # [start:end], error handle, internal server error. try except 
 
 
 
--- service/tableNameService.py
+++ service/tableNameService.py
@@ -2,8 +2,8 @@ from repository.singleRepo import singleRepo
 # optional start and end ints
 # default 0 , 1000
 # 
-def tableNameService(tableName:str)->object:
+def tableNameService(tableName:str, start:int = 0, end:int = 1000)->object:
   if tableName == "data_dict" or tableName == "fdic_fail":
-    return singleRepo(tableName) # pass start , int, required
+    return singleRepo(tableName, start, end) # pass start , int, required
   tableNameComplete = "table_"+tableName
-  return singleRepo(tableNameComplete)
+  return singleRepo(tableNameComplete, start, end)
