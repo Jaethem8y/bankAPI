e7f6aa121d2e80eaf4a0788cd5243d808fb24c96
jaethem8 <jaehyeok.choi@drake.edu>
2022-02-11 10:13:18 -0600

    add redirect

--- main.py
+++ main.py
@@ -1,29 +1,24 @@
 from fastapi import FastAPI, Request
+from fastapi.responses import RedirectResponse
 from service.tableNameService import tableNameService
 from service.showTableService import showTableService
 from service.allTableNameService import allTableNameService
 from service.descriptionTableService import descriptionTableService
 from postModels.Limit import Limit
-from fastapi.responses import HTMLResponse
-from fastapi.staticfiles import StaticFiles
-from fastapi.templating import Jinja2Templates
+
 
 app = FastAPI()
-app.mount("/static", StaticFiles(directory="static"), name="static")
-templates = Jinja2Templates(directory="templates")
 
-
-
-@app.get(path="/",response_class=HTMLResponse)
+@app.get(path="/")
 async def getIndex(request:Request):
-  return templates.TemplateResponse("index.html",{"request":request})
+  return RedirectResponse("/docs")
 
 @app.get(path="/tables")
 async def getTableColName():
   return showTableService()
 
 @app.get(path="/{table_name}")
-async def getRowByTableName(table_name):
+async def getRowsByTableName(table_name):
   return tableNameService(table_name)
 
 @app.get(path="/all/{table_name}")
