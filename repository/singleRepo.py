597227a8e3220da5568d0a30d62f38a52be3a4c3
jaethem8 <jaehyeok.choi@drake.edu>
2022-02-17 15:18:27 -0600

    add search feature

--- repository/singleRepo.py
+++ repository/singleRepo.py
@@ -2,15 +2,16 @@ from sqlalchemy import MetaData, create_engine, Table, exc
 from sqlalchemy.orm import sessionmaker
 from key import key
 
-def singleRepo(table_name:str, start:int,end:int)->object:
+def singleRepo(table_name:str, start:int,end:int,search:str)->object:
   engine = create_engine(key)
   metadata = MetaData(engine)
   Session = sessionmaker(bind=engine)
   session = Session()
-  print("repo:",start,"end:",end)
+  print("repo:",start,"end:",end, "search", search)
   try:
     table = Table(table_name,metadata,autoload=True,autoload_with=engine)
-    return session.query(table)[start:end]
+    print("table_dis",table.c.meaning)
+    return session.query(table).filter(table.c.meaning.like("%"+search+"%"))[start:end]
   except exc.NoSuchTableError:
     return {"invalid query: table does not exist."}
 
--- service/tableNameService.py
+++ service/tableNameService.py
@@ -1,11 +1,11 @@
 from repository.singleRepo import singleRepo
 
-def tableNameService(table_name:str, start:int,end:int)->object:
+def tableNameService(table_name:str, start:int,end:int,search:str)->object:
   if int(end) - int(start) > 10000 or end == -1 or end <= start:
     end = start + 10000
   print("service:",start,"end:",end)
   if table_name == "data_dict" or table_name == "fdic_fail":
-    return singleRepo(table_name, start, end) 
+    return singleRepo(table_name, start, end, search) 
   else:
     tableNameComplete = "table_"+table_name
-    return singleRepo(tableNameComplete, start, end)
+    return singleRepo(tableNameComplete, start, end, search)
