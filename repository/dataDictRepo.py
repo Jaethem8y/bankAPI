6a291dd9e81ff18a1a4ee80b98c4ddb6ba4b254d
jaethem8 <jaehyeok.choi@drake.edu>
2022-03-22 17:06:48 -0500

    fix data_dict returning one big object

--- repository/dataDictRepo.py
+++ repository/dataDictRepo.py
@@ -9,15 +9,7 @@ def dataDictRepo(item_code:str,meaning:str)->object:
   session = Session()
   try:
     table = Table("data_dict",metadata,autoload=True,autoload_with=engine)
-    if item_code == "" and meaning == "":
-      ret = session.query(table)
-    elif item_code == "":
-      ret = session.query(table).filter(table.c["meaning"].like("%"+meaning+"%"))
-    elif meaning == "":
-      ret = session.query(table).filter(table.c["item_code"].like("%"+item_code+"%"))
-
-    else:
-      ret = session.query(table).filter(table.c["item_code"].like("%"+item_code+"%")).filter(table.c["meaning"].like("%"+meaning+"%"))
+    ret = session.query(table).filter(table.c["item_code"].like("%"+item_code+"%")).filter(table.c["meaning"].like("%"+meaning+"%"))[0:]
     session.close()
     return ret
   except exc.NoSuchTableError:
