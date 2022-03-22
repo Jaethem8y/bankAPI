c2309e746db5e568226789b1148314724c03a856
jaethem8 <jaehyeok.choi@drake.edu>
2022-03-22 16:07:52 -0500

    remove unnecessary code from tableNameService.py

--- service/tableNameService.py
+++ service/tableNameService.py
@@ -4,8 +4,7 @@ def tableNameService(table_name:str, start:int,end:int,search_col:str,search:str
   if int(end) - int(start) > 10000 or end == -1 or end <= start:
     end = start + 10000
   print("service:",start,"end:",end)
-  if table_name != "data_dict" and table_name != "fdic_fail":
-    table_name = "table_"+table_name
+  table_name = "table_"+table_name
   return singleRepo(table_name, start, end, search_col,search) 
 
 
